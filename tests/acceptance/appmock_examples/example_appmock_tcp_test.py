686491e437657febb892c26e4ad1fa904418dfa5
Łukasz Opioła <lukasz.opiola@gmail.com>
2015-06-11 12:42:46 +0200

    VFS-1149, new appmock tests

--- tests/acceptance/appmock_examples/example_appmock_tcp_test.py
+++ tests/acceptance/appmock_examples/example_appmock_tcp_test.py
@@ -34,65 +34,66 @@ class TestAppmockTCPExample:
         [container] = self.result['docker_ids']
         appmock_ip = docker.inspect(container)['NetworkSettings']['IPAddress'].encode(
             'ascii')
-
-        type1_message = 'test5\n'
-        type1_message_count = 5
-        type2_message = 'test12\n'
-        type2_message_count = 12
-        al_messages_count = type1_message_count + type2_message_count
-        message_to_client = 'test_message'
-
-        received = send_some_and_receive(appmock_ip, type1_message, type1_message_count,
-                                         type2_message, type2_message_count, message_to_client)
-        # Now, we can verify if expected messages were sent by the tested code
-        assert type1_message_count == appmock_client.tcp_server_specific_message_count(appmock_ip, 5555, type1_message)
-        assert type2_message_count == appmock_client.tcp_server_specific_message_count(appmock_ip, 5555, type2_message)
-        assert al_messages_count == appmock_client.tcp_server_all_messages_count(appmock_ip, 5555)
-        # And if 'message' was received on the socket
-        assert message_to_client == received
-        # Reset counters
-        appmock_client.reset_tcp_server_history(appmock_ip)
-        assert 0 == appmock_client.tcp_server_specific_message_count(appmock_ip, 5555, type1_message)
-        assert 0 == appmock_client.tcp_server_specific_message_count(appmock_ip, 5555, type2_message)
-        assert 0 == appmock_client.tcp_server_all_messages_count(appmock_ip, 5555)
-        # And check again
-        received = send_some_and_receive(appmock_ip, type1_message, type1_message_count,
-                                         type2_message, type2_message_count, message_to_client)
-        assert type1_message_count == appmock_client.tcp_server_specific_message_count(appmock_ip, 5555, type1_message)
-        assert type2_message_count == appmock_client.tcp_server_specific_message_count(appmock_ip, 5555, type2_message)
-        assert al_messages_count == appmock_client.tcp_server_all_messages_count(appmock_ip, 5555)
-        assert message_to_client == received
-
+        # Send some requests, check counters, order appmock to send something to the client and receive it
+        send_some_and_receive(appmock_ip)
         # Send multiple messages to itself and verify if they were received
-        assert make_appmock_send_to_itself(appmock_ip)
-        # Check counter endpoint
+        make_appmock_send_to_itself(appmock_ip)
+        # Check waiting for messages
         wait_until_appmock_receives(appmock_ip)
+        # Check counter endpoint
+        use_counter_endpoint(appmock_ip)
 
 
 # An example code which could be verified using appmock
-def send_some_and_receive(appmock_ip, type1_message, type1_message_count,
-                          type2_message, type2_message_count, message_to_client):
+def send_some_and_receive(appmock_ip):
     # Lets assume we are testing a code that sends some messages on tcp port
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     ssl_sock = ssl.wrap_socket(s)
     ssl_sock.settimeout(10)
     ssl_sock.connect((appmock_ip, 5555))
 
-    for i in range(type1_message_count):
-        ssl_sock.send(type1_message)
-    for i in range(type2_message_count):
-        ssl_sock.send(type2_message)
-    # TODO jesli nie ma tego sleepa to jest jakies zakleszczenie
-    # nie rozumiem dlaczego. Pewnie dlatego ze z tego samego procesu
-    # wysylam blokujace zadanie, zeby mi cos server przyslal.
-    time.sleep(1)
-    # We are running the test and tested code in the same process, so lets
-    # send data from here. Normally, it would be sent from test code.
-    appmock_client.tcp_server_send(appmock_ip, 5555, message_to_client, 1)
-    # Now receive something and return it
-    result = ssl_sock.recv()
+    type1_message = 'test5\n'
+    type1_message_count = 5
+    type2_message = 'test12\n'
+    type2_message_count = 12
+    all_messages_count = type1_message_count + type2_message_count
+    message_to_client = 'test_message'
+
+    def send_some():
+        for i in range(type1_message_count):
+            ssl_sock.send(type1_message)
+        for i in range(type2_message_count):
+            ssl_sock.send(type2_message)
+        # TODO jesli nie ma tego sleepa to jest jakies zakleszczenie
+        # nie rozumiem dlaczego. Pewnie dlatego ze z tego samego procesu
+        # wysylam blokujace zadanie, zeby mi cos server przyslal.
+        time.sleep(1)
+        # We are running the test and tested code in the same process, so lets
+        # send data from here. Normally, it would be sent from test code.
+        appmock_client.tcp_server_send(appmock_ip, 5555, message_to_client, 1)
+        # Now receive something and return it
+        result = ssl_sock.recv()
+        return result
+
+    received = send_some()
+    # Now, we can verify if expected messages were sent by the tested code
+    assert type1_message_count == appmock_client.tcp_server_specific_message_count(appmock_ip, 5555, type1_message)
+    assert type2_message_count == appmock_client.tcp_server_specific_message_count(appmock_ip, 5555, type2_message)
+    assert all_messages_count == appmock_client.tcp_server_all_messages_count(appmock_ip, 5555)
+    # And if 'message' was received on the socket
+    assert message_to_client == received
+    # Reset counters
+    appmock_client.reset_tcp_server_history(appmock_ip)
+    assert 0 == appmock_client.tcp_server_specific_message_count(appmock_ip, 5555, type1_message)
+    assert 0 == appmock_client.tcp_server_specific_message_count(appmock_ip, 5555, type2_message)
+    assert 0 == appmock_client.tcp_server_all_messages_count(appmock_ip, 5555)
+    # And check again
+    received = send_some()
+    assert type1_message_count == appmock_client.tcp_server_specific_message_count(appmock_ip, 5555, type1_message)
+    assert type2_message_count == appmock_client.tcp_server_specific_message_count(appmock_ip, 5555, type2_message)
+    assert all_messages_count == appmock_client.tcp_server_all_messages_count(appmock_ip, 5555)
+    assert message_to_client == received
     ssl_sock.close()
-    return result
 
 
 # Connects to appmock as TCP client and orders appmock to send 1000 messages to it, verifies if they came
@@ -107,7 +108,7 @@ def make_appmock_send_to_itself(appmock_ip):
     for i in range(1000):
         result = result and 'message' == ssl_sock.recv()
     ssl_sock.close()
-    return result
+    assert result == True
 
 
 # Connects to appmock as TCP client and sends 1000 requests, waits for them to be received.
@@ -121,4 +122,26 @@ def wait_until_appmock_receives(appmock_ip):
         ssl_sock.send('test2\n')
     appmock_client.tcp_server_wait_for_specific_messages(appmock_ip, 5555, 'test1\n', 1000, False, 10)
     appmock_client.tcp_server_wait_for_specific_messages(appmock_ip, 5555, 'test2\n', 1000, False, 10)
-    appmock_client.tcp_server_wait_for_any_messages(appmock_ip, 5555, 2000, False, 10)
+    appmock_client.reset_tcp_server_history(appmock_ip)
+    for i in range(1000):
+        ssl_sock.send('sdfg\n')
+        ssl_sock.send('adfgsdf\n')
+        ssl_sock.send('345rthas\n')
+        ssl_sock.send('367sth\n')
+        ssl_sock.send('qatert547\n')
+    appmock_client.tcp_server_wait_for_any_messages(appmock_ip, 5555, 5000, False, 10)
+
+
+# Connects to appmock as TCP client and sends 100000 requests on counter endpoint, waits for them to be received.
+def use_counter_endpoint(appmock_ip):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ssl_sock = ssl.wrap_socket(s)
+    ssl_sock.settimeout(10)
+    ssl_sock.connect((appmock_ip, 6666))
+    for i in range(20000):
+        ssl_sock.send('sdfg\n')
+        ssl_sock.send('adfgsdf\n')
+        ssl_sock.send('345rthas\n')
+        ssl_sock.send('367sth\n')
+        ssl_sock.send('qatert547\n')
+    appmock_client.tcp_server_wait_for_any_messages(appmock_ip, 6666, 100000, False, 10)
