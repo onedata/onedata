f1cdda9df1f0fd330a88bb21fc6344260262c37d
Jakub Kudzia <jakub.kudzia@outlook.com>
2016-03-02 15:36:47 +0100

    VFS-1684 - call env_up.py in env_up test

--- tests/acceptance/env_tests/env_up_test.py
+++ tests/acceptance/env_tests/env_up_test.py
@@ -3,6 +3,9 @@ from tests import test_utils
 import socket
 import time
 import os
+import subprocess
+import script_up_test
+import ast
 
 from environment import docker, env
 
@@ -12,7 +15,13 @@ class TestEnvUp:
     # Run the evn_up.py script, capture and parse the output
     def setup_class(cls):
         logdir = get_logdir_name(acceptance_logdir, get_test_name(__file__))
-        cls.result = env.up(test_utils.test_file('env.json'), logdir=logdir)
+        result = subprocess.check_output([
+            os.path.join(docker_dir, "env_up.py"),
+            test_utils.test_file('env.json'),
+            '-l', logdir
+        ])
+        stripped_result = script_up_test.strip_output_logs(result)
+        cls.result = ast.literal_eval(stripped_result)
 
     @classmethod
     # Clean up removing all dockers created in the test
@@ -21,7 +30,7 @@ class TestEnvUp:
 
     # Test if the env_up.py script works as expected.
     def test_env_up(self):
-        res = self.result
+        env = self.result
         # Check if number of started dockers is correct
         # The number should be:
         # 1 dns node
@@ -38,55 +47,10 @@ class TestEnvUp:
         # 2 client nodes
         # ------------
         # 15 nodes
-        assert 15 == len(res['docker_ids'])
-        # Get the DNS ip
-        dns = res['dns']
-        # Will throw if the dns address is not legal
-        socket.inet_aton(dns)
-        # Check connectivity to system components using the DNS
-        # Check GR nodes
-        # gr_node is in form name@name.timestamp.dev.docker
-        for gr_node in res['gr_nodes']:
-            (gr_name, sep, gr_hostname) = gr_node.partition('@')
-            gr_ip = test_utils.dns_lookup(gr_hostname, dns)
-            assert test_utils.ping(gr_ip)
-            assert test_utils.check_http_connectivity(gr_ip, 443, '/', 200, number_of_retries=50)
+        assert 15 == len(env['docker_ids'])
 
-        # Check GR DB nodes
-        # gr_db_node is in form name@name.timestamp.dev.docker
-        for gr_db_node in res['gr_db_nodes']:
-            (gr_db_name, sep, gr_db_hostname) = gr_db_node.partition('@')
-            gr_db_ip = test_utils.dns_lookup(gr_db_hostname, dns)
-            assert test_utils.ping(gr_db_ip)
-            assert test_utils.check_http_connectivity(gr_db_ip, 5984, '/_utils/', 200, use_ssl=False, number_of_retries=50)
-
-        # Check OP CM nodes
-        # cm_node is in form name@name.timestamp.dev.docker
-        for cm_node in res['cluster_manager_nodes']:
-            (cm_name, sep, cm_hostname) = cm_node.partition('@')
-            cm_ip = test_utils.dns_lookup(cm_hostname, dns)
-            assert test_utils.ping(cm_ip)
-
-        # Check OP worker nodes
-        # w_node is in form name@name.timestamp.dev.docker
-        for w_node in res['op_worker_nodes']:
-            (w_name, sep, w_hostname) = w_node.partition('@')
-            w_ip = test_utils.dns_lookup(w_hostname, dns)
-            assert test_utils.ping(w_ip)
-            assert test_utils.check_http_connectivity(w_ip, 6666, '/nagios', 200, use_ssl=False, number_of_retries=50)
-
-        # Check appmock nodes
-        # am_node is in form name@name.timestamp.dev.docker
-        for am_node in res['appmock_nodes']:
-            (am_name, sep, am_hostname) = am_node.partition('@')
-            am_ip = test_utils.dns_lookup(am_hostname, dns)
-            assert test_utils.ping(am_ip)
-            assert test_utils.check_http_connectivity(am_ip, 443, '/test2', 200, number_of_retries=50)
-
-        # Check client nodes
-        # oc_node is in form name.timestamp.dev.docker
-        for oc_node in res['client_nodes']:
-            oc_ip = test_utils.dns_lookup(oc_node, dns)
-            assert test_utils.ping(oc_ip)
-
-
+        script_up_test.check_globalregistry_up(env, 1)
+        script_up_test.check_cluster_manager_up(env, 2)
+        script_up_test.check_provider_worker_up(env, 4)
+        script_up_test.check_appmock_up(env, 2)
+        script_up_test.check_client_up(env, 2)
