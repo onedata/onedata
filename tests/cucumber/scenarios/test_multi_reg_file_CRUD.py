c60e3c2abb33105de2781c776358cc6111492312
Jakub Kudzia <jakub.kudzia@outlook.com>
2016-05-05 14:22:27 +0200

    VFS-1881 - test_reg_file_CRUD checked, failing tests marked as expected to fail

--- tests/cucumber/scenarios/test_multi_reg_file_CRUD.py
+++ tests/cucumber/scenarios/test_multi_reg_file_CRUD.py
@@ -17,30 +17,33 @@ from steps.multi_reg_file_steps import *
 from steps.common import *
 
 
-# @scenario(
-#     '../features/multi_reg_file_CRUD.feature',
-#     'Create regular file'
-# )
-# def test_create(env_description_file):
-#     pass
-#
-#
-# @scenario(
-#     '../features/multi_reg_file_CRUD.feature',
-#     'Rename regular file without permission'
-# )
-# def test_rename_without_permission(env_description_file):
-#     pass
-#
-#
-# @scenario(
-#     '../features/multi_reg_file_CRUD.feature',
-#     'Rename regular file with permission'
-# )
-# def test_rename_with_permission(env_description_file):
-#     pass
+@scenario(
+    '../features/multi_reg_file_CRUD.feature',
+    'Create regular file'
+)
+def test_create(env_description_file):
+    pass
 
 
+@scenario(
+    '../features/multi_reg_file_CRUD.feature',
+    'Rename regular file without permission'
+)
+def test_rename_without_permission(env_description_file):
+    pass
+
+
+@scenario(
+    '../features/multi_reg_file_CRUD.feature',
+    'Rename regular file with permission'
+)
+def test_rename_with_permission(env_description_file):
+    pass
+
+
+@pytest.mark.xfail_env(
+        envs=["env", "env2", "env3"],
+        reason="delete fails with permission denies")
 @scenario(
     '../features/multi_reg_file_CRUD.feature',
     'Delete regular file by owner'
@@ -49,125 +52,125 @@ def test_delete_by_owner(env_description_file):
     pass
 
 
-# @scenario(
-#         '../features/multi_reg_file_CRUD.feature',
-#         'Delete regular file by other user'
-# )
-# def test_delete_by_other_user(env_description_file):
-#     pass
-#
-#
-# # # TODO VFS-1822
-# @pytest.mark.xfail_env(
-#         envs=["env", "env2", "env3"],
-#         reason="reading fails in oneclient")
-# @scenario(
-#         '../features/multi_reg_file_CRUD.feature',
-#         'Read and write to regular file'
-# )
-# def test_read_write(env_description_file):
-#     pass
-#
-#
-# # # TODO VFS-1822
-# @pytest.mark.xfail_env(
-#         envs=["env", "env2", "env3"],
-#         reason="reading fails in oneclient")
-# @scenario(
-#         '../features/multi_reg_file_CRUD.feature',
-#         'Read regular file without read permission'
-# )
-# def test_read_without_permission(env_description_file):
-#     pass
-#
-#
-# # # TODO VFS-1510
-# @pytest.mark.xfail_env(
-#         envs=["env", "env2", "env3"],
-#         reason="reading fails in oneclient")
-# @scenario(
-#         '../features/multi_reg_file_CRUD.feature',
-#         'Write to regular file with write permission'
-# )
-# def test_write_with_permission(env_description_file):
-#     pass
-#
-#
-# @scenario(
-#         '../features/multi_reg_file_CRUD.feature',
-#         'Write to regular file without write permission'
-# )
-# def test_write_without_permission(env_description_file):
-#     pass
-#
-#
-# # # TODO - VFS-1512
-# @pytest.mark.xfail_env(
-#         envs=["env", "env2", "env3"],
-#         reason="reading fails in oneclient")
-# @scenario(
-#         '../features/multi_reg_file_CRUD.feature',
-#         'Execute file with execute permission'
-# )
-# def test_execute_with_permission(env_description_file):
-#     pass
-#
-#
-# # # TODO - VFS-1512
-# @pytest.mark.xfail_env(
-#         envs=["env", "env2", "env3"],
-#         reason="reading fails in oneclient")
-# @scenario(
-#         '../features/multi_reg_file_CRUD.feature',
-#         'Execute file without execute permission'
-# )
-# def test_execute_without_permission(env_description_file):
-#     pass
-#
-#
-# # # TODO VFS-1823
-# @pytest.mark.xfail_env(
-#         envs=["env", "env2", "env3"],
-#         reason="reading fails in oneclient")
-# @scenario(
-#         '../features/multi_reg_file_CRUD.feature',
-#         'Move regular file and read'
-# )
-# def test_move(env_description_file):
-#     pass
-#
-#
-# # # TODO VFS-1513
-# @pytest.mark.xfail_env(
-#         envs=["env", "env2", "env3"],
-#         reason="reading fails in oneclient")
-# @scenario(
-#     '../features/multi_reg_file_CRUD.feature',
-#     'Move big regular file and check MD5'
-# )
-# def test_move_big(env_description_file):
-#     pass
-#
-#
-# # # # TODO VFS-1822
-# @pytest.mark.xfail_env(
-#         envs=["env", "env2", "env3"],
-#         reason="reading fails in oneclient")
-# @scenario(
-#         '../features/multi_reg_file_CRUD.feature',
-#         'Copy regular file and read'
-# )
-# def test_copy(env_description_file):
-#     pass
-#
-#
-# # # TODO VFS-1513
-# @pytest.mark.xfail_env(
-#         envs=["env", "env2", "env3"],
-#         reason="reading fails in oneclient")
-# @scenario(
-#         '../features/multi_reg_file_CRUD.feature',
-#         'Copy big regular file and check MD5'
-# )
-# def test_copy_big(env_description_file):
-#     pass
+@scenario(
+        '../features/multi_reg_file_CRUD.feature',
+        'Delete regular file by other user'
+)
+def test_delete_by_other_user(env_description_file):
+    pass
+
+
+# # TODO VFS-1822
+@pytest.mark.xfail_env(
+        envs=["env", "env2", "env3"],
+        reason="reading fails in oneclient")
+@scenario(
+        '../features/multi_reg_file_CRUD.feature',
+        'Read and write to regular file'
+)
+def test_read_write(env_description_file):
+    pass
+
+
+# # TODO VFS-1822
+@pytest.mark.xfail_env(
+        envs=["env", "env2", "env3"],
+        reason="reading fails in oneclient")
+@scenario(
+        '../features/multi_reg_file_CRUD.feature',
+        'Read regular file without read permission'
+)
+def test_read_without_permission(env_description_file):
+    pass
+
+
+# # TODO VFS-1510
+@pytest.mark.xfail_env(
+        envs=["env", "env2", "env3"],
+        reason="reading fails in oneclient")
+@scenario(
+        '../features/multi_reg_file_CRUD.feature',
+        'Write to regular file with write permission'
+)
+def test_write_with_permission(env_description_file):
+    pass
+
+
+@scenario(
+        '../features/multi_reg_file_CRUD.feature',
+        'Write to regular file without write permission'
+)
+def test_write_without_permission(env_description_file):
+    pass
+
+
+# # TODO - VFS-1512
+@pytest.mark.xfail_env(
+        envs=["env", "env2", "env3"],
+        reason="reading fails in oneclient")
+@scenario(
+        '../features/multi_reg_file_CRUD.feature',
+        'Execute file with execute permission'
+)
+def test_execute_with_permission(env_description_file):
+    pass
+
+
+# # TODO - VFS-1512
+@pytest.mark.xfail_env(
+        envs=["env", "env2", "env3"],
+        reason="reading fails in oneclient")
+@scenario(
+        '../features/multi_reg_file_CRUD.feature',
+        'Execute file without execute permission'
+)
+def test_execute_without_permission(env_description_file):
+    pass
+
+
+# # TODO VFS-1823
+@pytest.mark.xfail_env(
+        envs=["env", "env2", "env3"],
+        reason="reading fails in oneclient")
+@scenario(
+        '../features/multi_reg_file_CRUD.feature',
+        'Move regular file and read'
+)
+def test_move(env_description_file):
+    pass
+
+
+# # TODO VFS-1513
+@pytest.mark.xfail_env(
+        envs=["env", "env2", "env3"],
+        reason="reading fails in oneclient")
+@scenario(
+    '../features/multi_reg_file_CRUD.feature',
+    'Move big regular file and check MD5'
+)
+def test_move_big(env_description_file):
+    pass
+
+
+# # TODO VFS-1822
+@pytest.mark.xfail_env(
+        envs=["env", "env2", "env3"],
+        reason="reading fails in oneclient")
+@scenario(
+        '../features/multi_reg_file_CRUD.feature',
+        'Copy regular file and read'
+)
+def test_copy(env_description_file):
+    pass
+
+
+# # TODO VFS-1513
+@pytest.mark.xfail_env(
+        envs=["env", "env2", "env3"],
+        reason="reading fails in oneclient")
+@scenario(
+        '../features/multi_reg_file_CRUD.feature',
+        'Copy big regular file and check MD5'
+)
+def test_copy_big(env_description_file):
+    pass
