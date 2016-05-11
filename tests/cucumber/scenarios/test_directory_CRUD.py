fd7f2a7798dc7a1bec01648c1f2c38e364ca6337
Jakub Kudzia <jakub.kudzia@outlook.com>
2016-05-11 13:16:01 +0200

    VFS-1918 - uncomment tests

--- tests/cucumber/scenarios/test_directory_CRUD.py
+++ tests/cucumber/scenarios/test_directory_CRUD.py
@@ -21,187 +21,187 @@ from pytest_bdd import scenario
 )
 def test_create(env_description_file):
     pass
-#
-#
-# @scenario(
-#     '../features/directory_CRUD.feature',
-#     'Create directory in default space'
-# )
-# def test_create_default_spaces(env_description_file):
-#     pass
-#
-#
-# @scenario(
-#     '../features/directory_CRUD.feature',
-#     'Create directory in non-default space'
-# )
-# def test_create_in_spaces(env_description_file):
-#     pass
-#
-#
-# @scenario(
-#     '../features/directory_CRUD.feature',
-#     'Create directory spaces'
-# )
-# def test_create_spaces_dir(env_description_file):
-#     pass
-#
-#
-# @scenario(
-#     '../features/directory_CRUD.feature',
-#     'Create space'
-# )
-# def test_create_space(env_description_file):
-#     pass
-#
-#
-# @scenario(
-#     '../features/directory_CRUD.feature',
-#     'Rename directory'
-# )
-# def test_rename(env_description_file):
-#     pass
-#
-#
-# @scenario(
-#     '../features/directory_CRUD.feature',
-#     'Delete empty directory'
-# )
-# def test_delete(env_description_file):
-#     pass
-#
-#
-# @scenario(
-#     '../features/directory_CRUD.feature',
-#     'Delete directory spaces'
-# )
-# def test_delete_spaces_dir(env_description_file):
-#     pass
-#
-#
-# @scenario(
-#     '../features/directory_CRUD.feature',
-#     'Delete space'
-# )
-# def test_delete_space(env_description_file):
-#     pass
-#
-#
-# @scenario(
-#     '../features/directory_CRUD.feature',
-#     'Child directories'
-# )
-# def test_children(env_description_file):
-#     pass
-#
-#
-# @scenario(
-#     '../features/directory_CRUD.feature',
-#     'Child directories 2'
-# )
-# def test_children2(env_description_file):
-#     pass
-#
-#
-# @scenario(
-#     '../features/directory_CRUD.feature',
-#     'Duplication'
-# )
-# def test_duplication(env_description_file):
-#     pass
-#
-#
-# @scenario(
-#     '../features/directory_CRUD.feature',
-#     'Duplication in spaces'
-# )
-# def test_duplication_spaces(env_description_file):
-#     pass
-#
-#
-# @scenario(
-#     '../features/directory_CRUD.feature',
-#     'Delete empty directory and parents'
-# )
-# def test_delete_parents(env_description_file):
-#     pass
-#
-#
-# @scenario(
-#     '../features/directory_CRUD.feature',
-#     'Delete non-empty directory in wrong way'
-# )
-# def test_delete_non_empty_wrong(env_description_file):
-#     pass
-#
-#
-# @scenario(
-#     '../features/directory_CRUD.feature',
-#     'Delete non-empty directory'
-# )
-# def test_delete_non_empty(env_description_file):
-#     pass
-#
-#
-# @scenario(
-#     '../features/directory_CRUD.feature',
-#     'Move directory'
-# )
-# def test_move(env_description_file):
-#     pass
-#
-#
-# @scenario(
-#     '../features/directory_CRUD.feature',
-#     'Copy directory'
-# )
-# def test_copy(env_description_file):
-#     pass
-#
-#
-# @scenario(
-#     '../features/directory_CRUD.feature',
-#     'Move directory to itself'
-# )
-# def test_move_to_itself(env_description_file):
-#     pass
-#
-#
-# @scenario(
-#     '../features/directory_CRUD.feature',
-#     'Move directory to its subtree'
-# )
-# def test_move_to_subtree(env_description_file):
-#     pass
-#
-#
-# @scenario(
-#     '../features/directory_CRUD.feature',
-#     'Move directory to itself in spaces'
-# )
-# def test_move_to_itself_spaces(env_description_file):
-#     pass
-#
-#
-# @scenario(
-#     '../features/directory_CRUD.feature',
-#     'Move directory to itself in default space'
-# )
-# def test_move_to_itself_default_space(env_description_file):
-#     pass
-#
-#
-# @scenario(
-#     '../features/directory_CRUD.feature',
-#     'Move directory to its subtree in spaces'
-# )
-# def test_move_to_subtree_spaces(env_description_file):
-#     pass
-#
-#
-# @scenario(
-#     '../features/directory_CRUD.feature',
-#     'Move directory to its subtree in default space'
-# )
-# def test_move_to_subtree_default_space(env_description_file):
-#     pass
+
+
+@scenario(
+    '../features/directory_CRUD.feature',
+    'Create directory in default space'
+)
+def test_create_default_spaces(env_description_file):
+    pass
+
+
+@scenario(
+    '../features/directory_CRUD.feature',
+    'Create directory in non-default space'
+)
+def test_create_in_spaces(env_description_file):
+    pass
+
+
+@scenario(
+    '../features/directory_CRUD.feature',
+    'Create directory spaces'
+)
+def test_create_spaces_dir(env_description_file):
+    pass
+
+
+@scenario(
+    '../features/directory_CRUD.feature',
+    'Create space'
+)
+def test_create_space(env_description_file):
+    pass
+
+
+@scenario(
+    '../features/directory_CRUD.feature',
+    'Rename directory'
+)
+def test_rename(env_description_file):
+    pass
+
+
+@scenario(
+    '../features/directory_CRUD.feature',
+    'Delete empty directory'
+)
+def test_delete(env_description_file):
+    pass
+
+
+@scenario(
+    '../features/directory_CRUD.feature',
+    'Delete directory spaces'
+)
+def test_delete_spaces_dir(env_description_file):
+    pass
+
+
+@scenario(
+    '../features/directory_CRUD.feature',
+    'Delete space'
+)
+def test_delete_space(env_description_file):
+    pass
+
+
+@scenario(
+    '../features/directory_CRUD.feature',
+    'Child directories'
+)
+def test_children(env_description_file):
+    pass
+
+
+@scenario(
+    '../features/directory_CRUD.feature',
+    'Child directories 2'
+)
+def test_children2(env_description_file):
+    pass
+
+
+@scenario(
+    '../features/directory_CRUD.feature',
+    'Duplication'
+)
+def test_duplication(env_description_file):
+    pass
+
+
+@scenario(
+    '../features/directory_CRUD.feature',
+    'Duplication in spaces'
+)
+def test_duplication_spaces(env_description_file):
+    pass
+
+
+@scenario(
+    '../features/directory_CRUD.feature',
+    'Delete empty directory and parents'
+)
+def test_delete_parents(env_description_file):
+    pass
+
+
+@scenario(
+    '../features/directory_CRUD.feature',
+    'Delete non-empty directory in wrong way'
+)
+def test_delete_non_empty_wrong(env_description_file):
+    pass
+
+
+@scenario(
+    '../features/directory_CRUD.feature',
+    'Delete non-empty directory'
+)
+def test_delete_non_empty(env_description_file):
+    pass
+
+
+@scenario(
+    '../features/directory_CRUD.feature',
+    'Move directory'
+)
+def test_move(env_description_file):
+    pass
+
+
+@scenario(
+    '../features/directory_CRUD.feature',
+    'Copy directory'
+)
+def test_copy(env_description_file):
+    pass
+
+
+@scenario(
+    '../features/directory_CRUD.feature',
+    'Move directory to itself'
+)
+def test_move_to_itself(env_description_file):
+    pass
+
+
+@scenario(
+    '../features/directory_CRUD.feature',
+    'Move directory to its subtree'
+)
+def test_move_to_subtree(env_description_file):
+    pass
+
+
+@scenario(
+    '../features/directory_CRUD.feature',
+    'Move directory to itself in spaces'
+)
+def test_move_to_itself_spaces(env_description_file):
+    pass
+
+
+@scenario(
+    '../features/directory_CRUD.feature',
+    'Move directory to itself in default space'
+)
+def test_move_to_itself_default_space(env_description_file):
+    pass
+
+
+@scenario(
+    '../features/directory_CRUD.feature',
+    'Move directory to its subtree in spaces'
+)
+def test_move_to_subtree_spaces(env_description_file):
+    pass
+
+
+@scenario(
+    '../features/directory_CRUD.feature',
+    'Move directory to its subtree in default space'
+)
+def test_move_to_subtree_default_space(env_description_file):
+    pass
