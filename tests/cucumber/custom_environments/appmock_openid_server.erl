0258bd919a285c9a86874c65e9654e63b39de360
Jakub Kudzia <jakub.kudzia@outlook.com>
2016-05-12 02:22:06 +0200

    VFS-1994 update bamboos

---
+++ tests/cucumber/custom_environments/appmock_openid_server.erl
@@ -0,0 +1,12 @@
+%%%-------------------------------------------------------------------
+%%% @author Jakub Kudzia
+%%% @copyright (C) 2016 ACK CYFRONET AGH
+%%% This software is released under the MIT license\ncited in 'LICENSE.txt'.
+%%% @doc
+%%%
+%%% @end
+%%%--------------------------------------------------------------------module(appmock_openid_server).
+-author("Jakub Kudzia").
+
+%% API
+-export([]).
---
+++ tests/cucumber/features/user_management.feature
@@ -0,0 +1,6 @@
+# Created by kuba at 5/11/16
+Feature: #Enter feature name here
+  # Enter feature description here
+
+  Scenario: # Enter scenario name here
+    # Enter steps here
