c3f7ce459e6dcca87390ba4a5132153c019c0c68
Jakub Liput <jakub.liput@gmail.com>
2016-07-05 09:55:30 +0200

    VFS-2002 Fixing invalid imports after merge

--- tests/cucumber/scenarios/test_multiprovider_space_management.py
+++ tests/cucumber/scenarios/test_multiprovider_space_management.py
@@ -12,7 +12,7 @@ from tests.cucumber.steps.user_steps import *
 from tests.cucumber.steps.multi_file_steps import *
 from tests.cucumber.steps.multi_reg_file_steps import *
 from tests.cucumber.steps.multi_dir_steps import *
-from tests.cucumber.steps.cucumber_utils import *
+from tests.utils.cucumber_utils import *
 from tests.utils.path_utils import env_file
 
 from pytest_bdd import scenario
