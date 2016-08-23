5f2aafa2f3dd490aebd5a2b55f59ce28105b1d4a
Krzysztof Trzepla <krzysztof.trzepla@gmail.com>
2016-08-23 18:03:25 +0200

    VFS-2156 Update packages tests

--- tests/packaging/deb/deb_install_test_data/deb_install_script.py
+++ tests/packaging/deb/deb_install_test_data/deb_install_script.py
@@ -1,7 +1,13 @@
 from __future__ import print_function
 
+import json
+import requests
 import sys
+import time
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from subprocess import STDOUT, check_call, check_output
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 # get packages
 packages = check_output(['ls', '/root/pkg']).split()
@@ -28,12 +34,12 @@ check_call(['locale-gen', 'en_US.UTF-8'])
 check_call(['apt-get', '-y', 'install', 'curl', 'apt-transport-https', 'wget'])
 
 # get couchbase
-check_call(['wget', 'http://packages.couchbase.com/releases/4.0.0/couchbase'
-                    '-server-community_4.0.0-ubuntu14.04_amd64.deb'])
+check_call(['wget', 'http://packages.couchbase.com/releases/4.1.0/couchbase'
+                    '-server-community_4.1.0-ubuntu14.04_amd64.deb'])
 
 # install
 check_call(['sh', '-c',
-            'dpkg -i couchbase-server-community_4.0.0-ubuntu14.04_amd64.deb '
+            'dpkg -i couchbase-server-community_4.1.0-ubuntu14.04_amd64.deb '
             '; apt-get -f -y install'
             ], stderr=STDOUT)
 check_call(['sh', '-c', 'dpkg -i /root/pkg/{package} ; apt-get -f -y '
@@ -57,7 +63,7 @@ check_call(['ls', '/etc/cluster_manager/app.config'])
 check_call(['ls', '/etc/op_worker/app.config'])
 check_call(['/usr/bin/oneclient', '--help'])
 
-# disable OZ cert verification
+# disable onezone certificate verification
 check_call(['sed', '-i', 's/{verify_oz_cert, true}/{verify_oz_cert, false}/g',
             '/etc/op_panel/app.config'])
 check_call(['service', 'op_panel', 'restart'])
@@ -68,13 +74,38 @@ check_call(['wget', '-O', '/etc/ssl/cert.pem',
             'ca-bundle.crt'])
 
 # oneprovider configure and install
-check_call(['op_panel_admin', '--install', '/root/data/install.yml'])
+with open('/root/data/install.yml', 'r') as f:
+    r = requests.post(
+        'https://127.0.0.1:9443/api/v3/onepanel/provider/configuration',
+        headers={'content-type': 'application/x-yaml'},
+        data=f.read(),
+        verify=False)
+
+    loc = r.headers['location']
+    status = 'running'
+    while status == 'running':
+        r = requests.get('https://127.0.0.1:9443' + loc,
+                         auth=('admin1', 'Password1'),
+                         verify=False)
+        print(r.text)
+        assert r.status_code == 200
+        status = json.loads(r.text)['status']
+        time.sleep(5)
+
+assert status == 'ok'
 
 # validate oneprovider is running
 check_call(['service', 'cluster_manager', 'status'])
 check_call(['service', 'op_worker', 'status'])
 
-# uninstall
-check_call(['op_panel_admin', '--uninstall'])
+# stop oneprovider services
+for service in ['workers', 'managers', 'databases']:
+    r = requests.patch(
+        'https://127.0.0.1:9443/api/v3/onepanel/provider/{0}?started=false'.format(
+            service),
+        auth=('admin1', 'Password1'),
+        headers={'content-type': 'application/json'},
+        verify=False)
+    assert r.status_code == 204
 
 sys.exit(0)
