4dd15ccea2b95aa88967572213c4256133aa297b
Jakub Kudzia <jakub.kudzia@outlook.com>
2016-11-21 12:33:39 +0100

    VFS-2725 - fix wrong keys in multiprovider_directio_env, uncomment xfail marks from test_multiprovider_replication

--- tests/acceptance/scenarios/test_multiprovider_replication.py
+++ tests/acceptance/scenarios/test_multiprovider_replication.py
@@ -29,73 +29,46 @@ def env_description_file(request):
     return absolute_path
 
 
-# TODO VFS-2792
-@pytest.mark.xfail_env(envs=['multiprovider_directio_env.json'],
-                       reason="Problem with luma")
 @scenario('Create files and see them on external provider')
 def test_create_and_list(env_description_file):
     pass
 
 
-# TODO VFS-2792
-@pytest.mark.xfail_env(envs=['multiprovider_directio_env.json'],
-                       reason="Problem with luma")
 @scenario('Create empty file and read it on external provider')
 def test_create_empty_and_read(env_description_file):
     pass
 
 
-# TODO VFS-2792
-@pytest.mark.xfail_env(envs=['multiprovider_directio_env.json'],
-                       reason="Problem with luma")
 @scenario('Write to file and check size on remote provider')
 def test_write_and_check_size(env_description_file):
     pass
 
 
-# TODO VFS-2792
-@pytest.mark.xfail_env(envs=['multiprovider_directio_env.json'],
-                       reason="Problem with luma")
 @scenario('Write to file and read on remote provider')
 def test_write_and_read(env_description_file):
     pass
 
 
-# TODO VFS-2792
-@pytest.mark.xfail_env(envs=['multiprovider_directio_env.json'],
-                       reason="Problem with luma")
 @scenario('Big file transfer with MD5 check')
 def test_big_transfer_and_md5_check(env_description_file):
     pass
 
 
-# TODO VFS-2792
-@pytest.mark.xfail_env(envs=['multiprovider_directio_env.json'],
-                       reason="Problem with luma")
 @scenario('Create nonempty file and override its contents on remote provider')
 def test_remote_file_override(env_description_file):
     pass
 
 
-# TODO VFS-2792
-@pytest.mark.xfail_env(envs=['multiprovider_directio_env.json'],
-                       reason="Problem with luma")
 @scenario('Create nonempty file and remove it on remote provider')
 def test_remote_file_removal(env_description_file):
     pass
 
 
-# TODO VFS-2792
-@pytest.mark.xfail_env(envs=['multiprovider_directio_env.json'],
-                       reason="Problem with luma")
 @scenario('Create nonempty file, append remotely, append locally and read both')
 def test_sequential_appends(env_description_file):
     pass
 
 
-# TODO VFS-2792
-@pytest.mark.xfail_env(envs=['multiprovider_directio_env.json'],
-                       reason="Problem with luma")
 @scenario('Concurrently write disjoint ranges and read the same on both providers')
 def test_conflict_on_disjoint_blocks(env_description_file):
     pass
