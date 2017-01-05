f8c908359657455678ea985e8ed1c3d33d2eb3c2
Bartek Walkowicz <wallko1@hotmail.com>
2017-01-05 16:05:34 +0100

    VFS-2551 refactor page objects and add one for provider drop panel

--- tests/gui/utils/onezone_gui.py
+++ tests/gui/utils/onezone_gui.py
@@ -3,10 +3,10 @@
 
 import re
 
-from pytest import fixture
 from selenium.common.exceptions import NoSuchElementException
 
 from tests.gui.utils.generic import repeat_failed, iter_ahead, find_web_elem
+
 
 __author__ = "Jakub Liput, Bartosz Walkowicz"
 __copyright__ = "Copyright (C) 2016 ACK CYFRONET AGH"
@@ -17,43 +17,30 @@ __license__ = "This software is released under the MIT license cited in " \
 RE_DATA_URL = re.compile(r'(?P<lang>/.*)?/data/(?P<space>.*)/(?P<dir>.*)')
 
 
-@fixture()
-def logout_button(selenium):
-    return selenium.find_element_by_css_selector('account-menu a#nav-home.logout')
-
-
 class OZPanelRecord(object):
+    _name_css = '.secondary-item-container .{}-header.truncate'
+    _home_icon_css = '.oneicon-{}-home'
+    _submenu_toggle_css = '.secondary-item-container {}'
 
     def __init__(self, web_elem, record_type, submenu_toggle='.clickable'):
         self.web_elem = web_elem
-        self.css_sels = {'name_header': '.secondary-item-container '
-                                        '.{}-header'
-                                        '.truncate'.format(record_type),
-                         'home_icon': '.oneicon-{}-home'.format(record_type),
-                         'submenu_toggle': '.secondary-item-container '
-                                           '{}'.format(submenu_toggle)}
-        self.err_msgs = {'name_header': 'cannot locate name header '
-                                        'for given {}'.format(record_type),
-                         'set_home': 'no home outline found for "{{}}" '
-                                     '{}'.format(record_type),
-                         'submenu_toggle': 'no space header found for {} '
-                                           'named "{{}}"'.format(record_type)}
+        self._name_css = self._name_css.format(record_type)
+        self._home_icon_css = self._home_icon_css.format(record_type)
+        self._submenu_toggle_css = self._submenu_toggle_css.format(submenu_toggle)
         self._type = record_type
 
     @property
     def name(self):
-        css_sel = self.css_sels['name_header']
-        err_msg = self.err_msgs['name_header']
-        header = find_web_elem(self.web_elem, css_sel, err_msg)
+        err_msg = 'cannot locate name header for given {}'.format(self._type)
+        header = find_web_elem(self.web_elem, self._name_css, err_msg)
         return header.text
 
     @property
     def is_home(self):
         css_sel1 = '.secondary-item-element.star-toggle .oneicon-home'
-        css_sel2 = self.css_sels['home_icon']
         try:
             self.web_elem.find_element_by_css_selector(css_sel1)
-            self.web_elem.find_element_by_css_selector(css_sel2)
+            self.web_elem.find_element_by_css_selector(self._home_icon_css)
         except NoSuchElementException:
             return False
         else:
@@ -62,7 +49,9 @@ class OZPanelRecord(object):
     def set_as_home(self):
         if not self.is_home:
             css = '.secondary-item-element.star-toggle .oneicon-home-outline'
-            err_msg = self.err_msgs['set_home'].format(self.name)
+            err_msg = 'no home outline found for ' \
+                      '"{name}" {type}'.format(name=self.name,
+                                               type=self._type)
             home_outline = find_web_elem(self.web_elem, css, err_msg)
             home_outline.click()
 
@@ -86,9 +75,9 @@ class OZPanelRecord(object):
         return True if (aria_expanded and 'true' == aria_expanded) else False
 
     def _get_submenu_toggle(self):
-        css_sel = self.css_sels['submenu_toggle']
-        err_msg = self.err_msgs['submenu_toggle'].format(self.name)
-        return find_web_elem(self.web_elem, css_sel, err_msg)
+        err_msg = 'no space header found for {type} ' \
+                  'named "{name}"'.format(type=self._type, name=self.name)
+        return find_web_elem(self.web_elem, self._submenu_toggle_css, err_msg)
 
 
 class SpaceRecord(OZPanelRecord):
@@ -100,20 +89,23 @@ class SpaceRecord(OZPanelRecord):
         @property
         def name(self):
             css_sel = '.one-label.truncate'
-            err_msg = 'unable to locate name header for supporting provider'
+            err_msg = 'unable to locate name header for supporting provider ' \
+                      'for given space in DATA SPACE MANAGEMENT oz panel'
             header = find_web_elem(self.web_elem, css_sel, err_msg)
             return header.text
 
         def click(self):
             css_sel = '.clickable'
-            err_msg = "can't click on '{}' supporting provider".format(self.name)
+            err_msg = "can't click on '{}' supporting provider for given space " \
+                      "in DATA SPACE MANAGEMENT oz panel".format(self.name)
             provider = find_web_elem(self.web_elem, css_sel, err_msg)
             provider.click()
 
         def unsupport_space(self):
             css_sel = '.clickable .oneicon-leave-space'
             err_msg = 'no unsupport space btn found for "{}" ' \
-                      'provider'.format(self.name)
+                      'provider for given space in ' \
+                      'DATA SPACE MANAGEMENT oz panel'.format(self.name)
             btn = find_web_elem(self.web_elem, css_sel, err_msg)
             btn.click()
 
@@ -125,7 +117,8 @@ class SpaceRecord(OZPanelRecord):
                     self.web_elem.find_elements_by_css_selector(css_sel))
         else:
             raise RuntimeError('submenu for space named "{}" '
-                               'is not expanded'.format(self.name))
+                               'is not expanded in DATA SPACE MANAGEMENT '
+                               'oz panel'.format(self.name))
 
     def __getitem__(self, provider_name):
         for provider in self.supporting_providers:
@@ -133,13 +126,15 @@ class SpaceRecord(OZPanelRecord):
                 return provider
         else:
             raise RuntimeError('no supporting provider named "{prov}" '
-                               'for space named "{space}" found'
-                               ''.format(prov=provider_name, space=self.name))
+                               'for space named "{space}" found in DATA SPACE '
+                               'MANAGEMENT oz panel'.format(prov=provider_name,
+                                                            space=self.name))
 
     @property
     def size(self):
         css_sel = '.secondary-item-element.clickable .space-header-size'
-        err_msg = 'no size label for space named "{}" found'.format(self.name)
+        err_msg = 'no size label for space named "{}" found in ' \
+                  'DATA SPACE MANAGEMENT oz panel'.format(self.name)
         size_label = find_web_elem(self.web_elem, css_sel, err_msg)
         return size_label.text
 
@@ -147,24 +142,27 @@ class SpaceRecord(OZPanelRecord):
     def providers_count(self):
         css_sel = '.secondary-item-element.providers-count .one-label'
         err_msg = 'no count supporting providers ' \
-                  'label for space named "{}" found'.format(self.name)
+                  'label for space named "{}" found in DATA SPACE MANAGEMENT ' \
+                  'oz panel'.format(self.name)
         count_label = find_web_elem(self.web_elem, css_sel, err_msg)
         return int(count_label.text)
 
-    class DropdownMenu(object):
+    class SpaceSupportTokenDropdownMenu(object):
         def __init__(self, web_elem):
             self.web_elem = web_elem
 
         @property
         def token(self):
             css_sel = 'input'
-            err_msg = 'no input box with token found for given dropdown'
+            err_msg = 'no input box with token found for ' \
+                      'given space support token dropdown'
             input_box = find_web_elem(self.web_elem, css_sel, err_msg)
             return input_box.get_attribute('value')
 
         def copy(self):
             css_sel = 'button'
-            err_msg = 'no copy button found for given dropdown'
+            err_msg = 'no copy button found for ' \
+                      'given space support token dropdown'
             btn = find_web_elem(self.web_elem, css_sel, err_msg)
             btn.click()
 
@@ -178,12 +176,13 @@ class SpaceRecord(OZPanelRecord):
             if 'open' in btn.get_attribute('class'):
                 css_sel2 = '{:s} .dropdown-menu'.format(css_sel)
                 err_msg2 = 'no dropdown menu after clicking on get support ' \
-                           'for "{}" space found'.format(sp_name)
+                           'for "{}" space found in ' \
+                           'DATA SPACE MANAGEMENT'.format(sp_name)
                 menu = find_web_elem(self.web_elem, css_sel2, err_msg2)
-                return SpaceRecord.DropdownMenu(menu)
+                return SpaceRecord.SpaceSupportTokenDropdownMenu(menu)
         else:
-            raise RuntimeError('submenu for space named "{}" '
-                               'is not expanded'.format(self.name))
+            raise RuntimeError('submenu for space named "{}" in DATA SPACE '
+                               'MANAGEMENT is not expanded'.format(self.name))
 
 
 class OZPanel(object):
@@ -231,11 +230,14 @@ class OZPanel(object):
 
 class DataSpaceManagementPanel(OZPanel):
 
-    def __getitem__(self, name):
+    @property
+    def spaces(self):
         css_sel = '.spaces-accordion-item'
-        spaces = (SpaceRecord(space, 'space') for space in
-                  self.web_elem.find_elements_by_css_selector(css_sel))
-        for space in spaces:
+        return (SpaceRecord(space, 'space') for space in
+                self.web_elem.find_elements_by_css_selector(css_sel))
+
+    def __getitem__(self, name):
+        for space in self.spaces:
             if name == space.name:
                 return space
         else:
@@ -262,6 +264,37 @@ class DataSpaceManagementPanel(OZPanel):
                                'in "{panel}" oz panel'.format(panel=self.name))
 
 
+class SpaceRecordInProvidersPanel(object):
+    def __init__(self, web_elem, name_css, size_css):
+        self.web_elem = web_elem
+        self.name_css = name_css
+        self.size_css = size_css
+
+    @property
+    def name(self):
+        err_msg = 'unable to locate name header for supported space in ' \
+                  'displayed provider panel'
+        header = find_web_elem(self.web_elem, self.name_css, err_msg)
+        return header.text
+
+    @property
+    def size(self):
+        err_msg = 'no size label for space named "{}" found ' \
+                  'in displayed provider panel'.format(self.name)
+        size_label = find_web_elem(self.web_elem, self.size_css, err_msg)
+        return size_label.text
+
+    @property
+    def is_home(self):
+        css_sel = '.oneicon-space-default'
+        try:
+            self.web_elem.find_element_by_css_selector(css_sel)
+        except NoSuchElementException:
+            return False
+        else:
+            return True
+
+
 class ProviderRecord(OZPanelRecord):
 
     @property
@@ -277,59 +310,38 @@ class ProviderRecord(OZPanelRecord):
     @property
     def spaces_count(self):
         css_sel = '.spaces-count'
-        err_msg = 'no spaces count for "{}" provider found'.format(self.name)
+        err_msg = 'no spaces count for "{}" provider found in ' \
+                  'GO TO YOUR FILES panel'.format(self.name)
         spaces_count = find_web_elem(self.web_elem, css_sel, err_msg)
         return int(spaces_count)
 
     def click_on(self):
         css_sel = '.secondary-item-container'
-        err_msg = 'no provider header for "{}" found'.format(self.name)
+        err_msg = 'no provider header for "{}" found in GO TO YOUR FILES ' \
+                  'panel'.format(self.name)
         header = find_web_elem(self.web_elem, css_sel, err_msg)
         header.click()
 
     def unset_from_home(self):
         css_sel = '.secondary-item-element.star-toggle .oneicon-home'
-        err_msg = 'no home icon found for "{}" provider'.format(self.name)
+        err_msg = 'no home icon found for "{}" provider in GO TO YOUR FILES ' \
+                  'panel'.format(self.name)
         home_icon = find_web_elem(self.web_elem, css_sel, err_msg)
         home_icon.click()
-
-    class SpaceRecord(object):
-        def __init__(self, web_elem):
-            self.web_elem = web_elem
-
-        @property
-        def name(self):
-            css_sel = '.one-label.truncate'
-            err_msg = 'unable to locate name header for supported space'
-            header = find_web_elem(self.web_elem, css_sel, err_msg)
-            return header.text
-
-        @property
-        def size(self):
-            css_sel = '.space-header-size'
-            err_msg = 'no size label for space named "{}" found'.format(self.name)
-            size_label = find_web_elem(self.web_elem, css_sel, err_msg)
-            return size_label.text
-
-        @property
-        def is_home(self):
-            css_sel = '.oneicon-space-default'
-            try:
-                self.web_elem.find_element_by_css_selector(css_sel)
-            except NoSuchElementException:
-                return False
-            else:
-                return True
 
     @property
     def supported_spaces(self):
         if self.is_submenu_expanded:
             css_sel = 'ul.tertiary-list li.sidebar-provider-space'
-            return (ProviderRecord.SpaceRecord(space) for space in
-                    self.web_elem.find_elements_by_css_selector(css_sel))
+            return (SpaceRecordInProvidersPanel(space,
+                                                name_css='.one-label.truncate',
+                                                size_css='.space-header-size')
+                    for space
+                    in self.web_elem.find_elements_by_css_selector(css_sel))
         else:
             raise RuntimeError('submenu for provider named "{}" '
-                               'is not expanded'.format(self.name))
+                               'is not expanded in GO TO YOUR FILES '
+                               'panel'.format(self.name))
 
     def __getitem__(self, space_name):
         for space in self.supported_spaces:
@@ -337,18 +349,23 @@ class ProviderRecord(OZPanelRecord):
                 return space
         else:
             raise RuntimeError('no supported space named "{space}" '
-                               'for provider named "{provider}" found'
-                               ''.format(provider=self.name, space=space_name))
+                               'for provider named "{provider}" in '
+                               'GO TO YOUR FILES panel '
+                               'found'.format(provider=self.name,
+                                              space=space_name))
 
 
 class GoToYourFilesPanel(OZPanel):
 
-    def __getitem__(self, name):
+    @property
+    def providers(self):
         css_sel = '#providers-list .providers-accordion-item'
-        providers = (ProviderRecord(provider, 'provider', '.spaces-count')
-                     for provider in
-                     self.web_elem.find_elements_by_css_selector(css_sel))
-        for provider in providers:
+        return (ProviderRecord(provider, 'provider', '.spaces-count')
+                for provider
+                in self.web_elem.find_elements_by_css_selector(css_sel))
+
+    def __getitem__(self, name):
+        for provider in self.providers:
             if name == provider.name:
                 return provider
         else:
@@ -408,3 +425,55 @@ class OZLoggedIn(object):
             for group, toggle in zip(items[::2], items[1::2]):
                 if panel == toggle.text.lower():
                     return cls(group)
+
+
+class ProviderDropPanel(object):
+
+    def __init__(self, web_elem):
+        self.web_elem = web_elem
+
+    @property
+    def name(self):
+        css_sel = '.title-label'
+        err_msg = 'no name found in displayed provider drop panel'
+        header = find_web_elem(self.web_elem, css_sel, err_msg)
+        return header.text
+
+    @property
+    def hostname(self):
+        css_sel = '.provider-host-text'
+        err_msg = 'no hostname found in displayed provider drop panel'
+        header = find_web_elem(self.web_elem, css_sel, err_msg)
+        return header.text
+
+    def copy_hostname(self):
+        css_sel = '.provider-host-copy-btn'
+        err_msg = 'no copy hostname btn found in displayed provider drop panel'
+        btn = find_web_elem(self.web_elem, css_sel, err_msg)
+        btn.click()
+
+    def go_to_your_files(self):
+        css_sel = '.drop-body button'
+        err_msg = "no 'Go to your files' btn found " \
+                  "in displayed provider drop panel"
+        btn = find_web_elem(self.web_elem, css_sel, err_msg)
+        btn.click()
+
+    @property
+    def supported_spaces(self):
+        css_sel = 'ul li.provider-place-drop-space'
+        return (SpaceRecordInProvidersPanel(space,
+                                            name_css='.space-label',
+                                            size_css='.space-size')
+                for space
+                in self.web_elem.find_elements_by_css_selector(css_sel))
+
+    def __getitem__(self, space_name):
+        for space in self.supported_spaces:
+            if space_name == space.name:
+                return space
+        else:
+            raise RuntimeError('no supported space named "{space}" '
+                               'for provider named "{provider}" in displayed '
+                               'drop panel found'.format(provider=self.name,
+                                                         space=space_name))
