d53852d3b15e7de8f511ccd543dfc5626a2818a1
Bartek Walkowicz <wallko1@hotmail.com>
2017-03-01 09:53:02 +0100

    VFS-2973 increase timeout waiting for files to load after refresh

--- tests/gui/steps/oneprovider/data_tab.py
+++ tests/gui/steps/oneprovider/data_tab.py
@@ -181,7 +181,7 @@ def wt_is_space_tree_root(selenium, browser_id, is_home, space_name, op_page):
                     'in data tab in Oneprovider page'))
 @then(parsers.parse('user of {browser_id} sees nonempty file browser '
                     'in data tab in Oneprovider page'))
-@repeat_failed(attempts=WAIT_BACKEND, timeout=True)
+@repeat_failed(attempts=WAIT_BACKEND*2, timeout=True)
 def assert_nonempty_file_browser_in_data_tab_in_op(selenium, browser_id,
                                                    op_page, tmp_memory):
     driver = select_browser(selenium, browser_id)
