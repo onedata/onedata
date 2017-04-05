eba1bdc6d4493774f95749188d7d435558509dde
Bartek Walkowicz <wallko1@hotmail.com>
2017-04-05 11:01:26 +0200

    VFS-3133 start refactoring page objects utils

--- tests/gui/utils/core/web_elements.py
+++ tests/gui/utils/core/web_elements.py
@@ -2,9 +2,11 @@
 """
 
 from abc import abstractmethod, ABCMeta
+from functools import partial
 
-from tests.gui.utils.core.web_objects import ButtonWebObject, WebObjectsSequence
-from tests.gui.utils.generic import find_web_elem, repeat_failed, find_web_elem_with_text
+from .web_objects import ButtonWebObject, WebObjectsSequence, InputWebObject, \
+    ButtonWithTextWebObject
+from tests.gui.utils.generic import find_web_elem, find_web_elem_with_text
 
 __author__ = "Bartosz Walkowicz"
 __copyright__ = "Copyright (C) 2017 ACK CYFRONET AGH"
@@ -16,7 +18,7 @@ class WebObject(type):
     def __init__(cls, cls_name, bases, cls_dict):
         for key, val in cls_dict.items():
             if isinstance(val, AbstractWebElement) and \
-                    (val.name is None or val.name == 'id'):
+                    (val.name in ('id', '')):
                 val.name = key
         super(WebObject, cls).__init__(cls_name, bases, cls_dict)
 
@@ -24,7 +26,7 @@ class WebObject(type):
 class AbstractWebElement(object):
     __metaclass__ = ABCMeta
 
-    def __init__(self, css_sel, name=None):
+    def __init__(self, css_sel, name=''):
         self.css_sel = css_sel
         self.name = name
 
@@ -43,19 +45,13 @@ class AbstractWebItem(AbstractWebElement):
     __metaclass__ = ABCMeta
 
     def __init__(self, *args, **kwargs):
-        if 'cls' in kwargs:
-            self.cls = kwargs.pop('cls')
-        else:
-            raise ValueError('cls argument not specified')
-
+        self.cls = keyword_only_arg(kwargs, 'cls')
         super(AbstractWebItem, self).__init__(*args, **kwargs)
 
 
 class WebElement(AbstractWebElement):
-    item_not_found_msg = '{item} element not found in {parent}'
-
     def __init__(self, *args, **kwargs):
-        self.parent_name = kwargs.pop('parent_name', None)
+        self.parent_name = kwargs.pop('parent_name', '')
         super(WebElement, self).__init__(*args, **kwargs)
 
     def __get__(self, instance, owner):
@@ -63,115 +59,55 @@ class WebElement(AbstractWebElement):
             return self
 
         return find_web_elem(instance.web_elem, self.css_sel,
-                             lambda: self._format_msg(self.item_not_found_msg,
-                                                      instance))
+                             lambda: self._format_msg('no {item} item found '
+                                                      'in {parent}', instance))
 
-    def _format_msg(self, msg_template, parent):
+    def _format_msg(self, err_msg, parent, **kwargs):
         name = self.name.replace('_', ' ').strip().upper()
-        parent_name = (self.parent_name
-                       if self.parent_name is not None
-                       else str(parent))
-        return msg_template.format(item=name, parent=parent_name)
+        p_name = (self.parent_name if self.parent_name != '' else str(parent))
+        return err_msg.format(item=name, parent=p_name, **kwargs)
 
 
-class WebElementsSequence(AbstractWebElement):
+class WebElementWithText(WebElement):
+    def __init__(self, *args, **kwargs):
+        self.text = keyword_only_arg(kwargs, 'text')
+        super(WebElementWithText, self).__init__(*args, **kwargs)
+
     def __get__(self, instance, owner):
         if instance is None:
             return self
 
-        return instance.web_elem.find_elements_by_css_selector(self.css_sel)
+        err_msg = 'no {item} with "{text}" text found in {parent}'
+        return find_web_elem_with_text(instance.web_elem,
+                                       self.css_sel, self.text,
+                                       lambda: self._format_msg(err_msg, instance,
+                                                                text=self.text))
 
 
 class WebItem(AbstractWebItem, WebElement):
-    item_not_found_msg = '{item} item not found in {parent}'
-
     def __get__(self, instance, owner):
         elem = super(WebItem, self).__get__(instance, owner)
-        if instance is None:
-            return elem
-        else:
-            return self.cls(instance.driver, elem, instance)
+        return elem if instance is None else self.cls(instance.driver,
+                                                      elem, parent=instance,
+                                                      name=self.name)
 
 
-class WebItemsSequence(AbstractWebItem, WebElementsSequence):
-    def __get__(self, instance, owner):
-        items = super(WebItemsSequence, self).__get__(instance, owner)
-        if instance is None:
-            return items
-        else:
-            return WebObjectsSequence(instance.driver, items,
-                                      instance, self.cls)
+Input = partial(WebItem, cls=InputWebObject)
+Button = partial(WebItem, cls=ButtonWebObject)
 
 
-class WebElementsSequenceItemWithText(object):
-    def __init__(self, seq, text, cls):
-        self.seq = seq
-        self.text = text.lower()
-        self.cls = cls
-
-    def __get__(self, instance, owner):
-        for item in self.seq.__get__(instance, owner):
-            if item.text.lower() == self.text:
-                return self.cls(instance.driver, item, instance)
+class WebItemWithText(WebItem, WebElementWithText):
+    pass
 
 
-class InputWebElement(WebElement):
-    item_not_found_msg = '{item} input box not found in {parent}'
-    typing_text_failed_msg = 'entering {value} to {item} input box in ' \
-                             '{parent} failed'
-
-    def __get__(self, instance, owner):
-        item = super(InputWebElement, self).__get__(instance, owner)
-        return item.get_attribute('value') if instance else item
-
-    @repeat_failed(attempts=10)
-    def __set__(self, instance, value):
-        input_box = super(InputWebElement, self).__get__(instance,
-                                                         type(instance))
-        input_box.clear()
-        input_box.send_keys(value)
-        assert self.__get__(instance, type(instance)) == value, \
-            lambda: self._format_msg(self.typing_text_failed_msg, instance)
+NamedButton = partial(WebItemWithText, cls=ButtonWithTextWebObject)
 
 
-class TextLabelWebElement(WebElement):
-    item_not_found_msg = '{item} label not found in {parent}'
-
-    def __get__(self, instance, owner):
-        item = super(TextLabelWebElement, self).__get__(instance, owner)
-        return item.text if instance else item
-
-
-class ButtonWebItem(WebItem):
-    item_not_found_msg = '{text} btn not found in {parent}'
-
-    def __init__(self, *args, **kwargs):
-        super(ButtonWebItem, self).__init__(cls=ButtonWebObject, *args, **kwargs)
-
-    def __get__(self, instance, owner):
-        btn = super(ButtonWebItem, self).__get__(instance, owner)
-        btn.name = self.name
-        return btn
-
-
-class IconWebElement(WebElement):
-    item_not_found_msg = '{item} icon not found in {parent}'
-
-
-class ToggleWebElement(WebElement):
-    item_not_found_msg = '{item} toggle not found in {parent}'
-
-
-class ButtonWebElement(WebElement):
-    item_not_found_msg = '{item} btn not found in {parent}'
-
-
-class HeaderWebElement(WebElement):
-    item_not_found_msg = '{item} header not found in {parent}'
-
-
-class ModalWebElement(WebElement):
-    item_not_found_msg = '{item} modal not found in {parent}'
+def keyword_only_arg(kwargs, arg):
+    try:
+        return kwargs.pop(arg)
+    except KeyError:
+        raise ValueError('{} argument not specified'.format(arg))
 
 
 class ButtonWithTextWebElement(WebElement):
@@ -195,9 +131,47 @@ class ButtonWithTextWebElement(WebElement):
                                        lambda: self._format_msg(msg, instance))
 
 
+class WebElementsSequence(AbstractWebElement):
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+
+        return instance.web_elem.find_elements_by_css_selector(self.css_sel)
+
+
+class WebItemsSequence(AbstractWebItem, WebElementsSequence):
+    def __get__(self, instance, owner):
+        items = super(WebItemsSequence, self).__get__(instance, owner)
+        if instance is None:
+            return items
+        else:
+            return WebObjectsSequence(instance.driver, items,
+                                      instance, self.cls)
+
+
 class ItemListWebElement(WebElement):
     def __get__(self, instance, owner):
         if instance is None:
             return self
 
         return instance.web_elem.find_elements_by_css_selector(self.css_sel)
+
+
+class WebElementsSequenceItemWithText(object):
+    def __init__(self, seq, text, cls):
+        self.seq = seq
+        self.text = text.lower()
+        self.cls = cls
+
+    def __get__(self, instance, owner):
+        for item in self.seq.__get__(instance, owner):
+            if item.text.lower() == self.text:
+                return self.cls(instance.driver, item, instance)
+
+
+class TextLabelWebElement(WebElement):
+    item_not_found_msg = '{item} label not found in {parent}'
+
+    def __get__(self, instance, owner):
+        item = super(TextLabelWebElement, self).__get__(instance, owner)
+        return item.text if instance else item
--- tests/gui/utils/core/web_objects.py
+++ tests/gui/utils/core/web_objects.py
@@ -3,7 +3,7 @@
 
 from abc import abstractmethod, ABCMeta
 
-from tests.gui.utils.generic import nth, click_on_web_elem
+from tests.gui.utils.generic import nth, click_on_web_elem, repeat_failed
 
 __author__ = "Bartosz Walkowicz"
 __copyright__ = "Copyright (C) 2017 ACK CYFRONET AGH"
@@ -14,14 +14,37 @@ __license__ = "This software is released under the MIT license cited in " \
 class AbstractWebObject(object):
     __metaclass__ = ABCMeta
 
-    def __init__(self, driver, web_elem, parent):
+    def __init__(self, driver, web_elem, parent=None, name=''):
         self.driver = driver
         self.web_elem = web_elem
         self.parent = parent
+        if name != '':
+            self.name = name
 
     @abstractmethod
     def __str__(self):
         pass
+
+
+class InputWebObject(AbstractWebObject):
+    name = 'input'
+
+    def __str__(self):
+        return '{} in {}'.format(self.name, self.parent)
+
+    @property
+    def value(self):
+        return self.web_elem.get_attribute('value')
+
+    @value.setter
+    @repeat_failed(attempts=10)
+    def value(self, val):
+        self.clear()
+        self.web_elem.send_keys(val)
+        assert self.value == val, 'entering "{}" to {} failed'.format(val, self)
+
+    def clear(self):
+        self.web_elem.clear()
 
 
 class ButtonWebObject(AbstractWebObject):
@@ -37,21 +60,29 @@ class ButtonWebObject(AbstractWebObject):
     def is_enabled(self):
         return self.web_elem.is_enabled()
 
-
-class NavigationButtonWebObject(ButtonWebObject):
-    name = 'button'
-
     def is_active(self):
         return 'active' in self.web_elem.get_attribute('class')
 
 
+class ButtonWithTextWebObject(ButtonWebObject):
+    def __str__(self):
+        return '{} btn with "{}" text in {}'.format(self.name, self.text,
+                                                    self.parent)
+
+    @property
+    def text(self):
+        return self.web_elem.text
+
+    id = text
+
+
 class WebObjectsSequence(object):
 
-    def __init__(self, driver, items, parent, cls):
+    def __init__(self, driver, items, cls, parent=None):
         self.driver = driver
         self.items = items
-        self.parent = parent
         self.cls = cls
+        self.parent = parent
 
     def _getitem_by_id(self, sel):
         for item in self:
@@ -92,6 +123,8 @@ class WebObjectsSequence(object):
             return None
 
     def __contains__(self, item):
+        if isinstance(item, self.cls):
+            item = item.id
         return False if self._getitem_by_id(item) is None else True
 
     def __len__(self):
@@ -101,7 +134,12 @@ class WebObjectsSequence(object):
         return len(self)
 
     def index(self, item_for_idx):
+        if isinstance(item_for_idx, self.cls):
+            item_searched = item_for_idx.id
+        else:
+            item_searched = item_for_idx
+
         for i, item in enumerate(self):
-            if item is item_for_idx:
+            if item.id == item_searched:
                 return i
         return None
