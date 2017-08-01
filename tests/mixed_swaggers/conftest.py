b3fef7b3f066a130ecbbc284085c8d946452d639
plgcwiertni <cwiertnia.michal@gmail.com>
2017-08-01 11:15:11 +0200

    VFS-3845 Started using GUI configuration.

---
+++ tests/mixed_swaggers/conftest.py
@@ -0,0 +1,24 @@
+"""
+Definitions of fixtures used in mixed tests using GUI and swagger.
+"""
+
+__author__ = "Michal Cwiertnia"
+__copyright__ = "Copyright (C) 2017 ACK CYFRONET AGH"
+__license__ = "This software is released under the MIT license cited in " \
+              "LICENSE.txt"
+
+pytest_plugins = "tests.gui.conftest"
+
+SELENIUM_IMPLICIT_WAIT = 4
+
+# use this const when using: WebDriverWait(selenium, WAIT_FRONTEND).until(lambda s: ...)
+# when waiting for frontend changes
+WAIT_FRONTEND = 4
+
+# use this const when using: WebDriverWait(selenium, WAIT_BACKEND).until(lambda s: ...)
+# when waiting for backend changes
+WAIT_BACKEND = 15
+
+# waiting for backend to load after refresh
+WAIT_REFRESH = WAIT_BACKEND
+MAX_REFRESH_COUNT = 8
