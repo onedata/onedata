02c259cdf6f7fd4ff766b014ea4b841f97a64a59
plgcwiertni <cwiertnia.michal@gmail.com>
2017-08-02 15:20:20 +0200

    VFS-3485 Improved step name

--- tests/mixed_swaggers/features/onezone_spaces.feature
+++ tests/mixed_swaggers/features/onezone_spaces.feature
@@ -9,8 +9,10 @@ Feature: Onezone spaces
     And user of browser logged as user1 to Onezone service
 
 
-  Scenario: User creates space using REST and sees in browser that new space has appeared
-    When "user1" creates space "helloworld"
+  Scenario: User creates space using REST API and sees in browser that it has appeared
+    When user of browser expands the "DATA SPACE MANAGEMENT" Onezone sidebar panel
+    And user of browser sees that there is no space named "helloworld" in expanded "DATA SPACE MANAGEMENT" Onezone panel
+    And user1 creates space "helloworld" in "z1" Onezone service
+    And user of browser refreshes site
+    And user of browser expands the "DATA SPACE MANAGEMENT" Onezone sidebar panel
     Then user of browser sees that space named "helloworld" has appeared in expanded "DATA SPACE MANAGEMENT" Onezone panel
-
-
--- tests/mixed_swaggers/steps/onezone_space_basic.py
+++ tests/mixed_swaggers/steps/onezone_space_basic.py
@@ -15,23 +15,21 @@ from tests.mixed_swaggers.onezone_client import (ApiClient as ApiClient_OZ,
 from tests import OZ_REST_PATH_PREFIX, OZ_REST_PORT
 
 
-@when(parsers.parse('"{username}" creates space "{space_name}"'))
-def create_spaces(username, space_name, hosts, users):
+@when(parsers.parse('{username} creates space "{space_name}" in "{zone_name}" Onezone service'))
+def create_spaces(username, space_name, zone_name, hosts, users):
+    Conf_OZ().verify_ssl = False
+    Conf_OZ().username = username
+    Conf_OZ().password = users[username].password
 
-        Conf_OZ().verify_ssl = False
-        Conf_OZ().username = username
-        Conf_OZ().password = users[username].password
+    user_client = ApiClient_OZ(host="https://{}:{}{}".format(
+        hosts['onezone'][zone_name],
+        OZ_REST_PORT,
+        OZ_REST_PATH_PREFIX),
+        header_name='authorization',
+        header_value=Conf_OZ().get_basic_auth_token())
 
-        user_client = ApiClient_OZ(host="https://{}:{}{}".
-                                   format(hosts['onezone']['z1'],
-                                          OZ_REST_PORT,
-                                          OZ_REST_PATH_PREFIX),
-                                   header_name='authorization',
-                                   header_value=Conf_OZ().
-                                   get_basic_auth_token())
+    user_api = UserApi(user_client)
 
-        user_api = UserApi(user_client)
-
-        new_space = Space()
-        new_space.name = space_name
-        user_api.create_user_space(new_space)
+    new_space = Space()
+    new_space.name = space_name
+    user_api.create_user_space(new_space)
