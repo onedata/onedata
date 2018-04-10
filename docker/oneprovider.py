3377a654098de07ad06bd46360ac8514769f8677
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-04-10 14:04:00 +0200

    VFS-4278 Start services on restart

--- docker/oneprovider.py
+++ docker/oneprovider.py
@@ -267,6 +267,50 @@ def print_logs(logs):
 
     return new_logs
 
+def is_configured(config):
+    users = get_users(config) + [('admin', 'password')]
+    r = do_request(users, requests.get,
+                   'https://127.0.0.1:9443/api/v3/onepanel/provider/configuration',
+                   verify=False)
+    if r.status_code == 200:
+        return True
+    elif r.status_code == 404:
+        return False
+    else:
+        raise ValueError('Error checking deployment status')
+
+
+def format_error(action, response):
+    return 'Error {action}: {error}\nDescription: {description}\n'
+    'Module: {module}\nFunction: {function}\nHosts: {hosts}\n'
+    'For more information please check the logs.'.format(
+        action=action,
+        error=response.get('error', 'unknown'),
+        description=response.get('description', '-'),
+        module=response.get('module', '-'),
+        function=response.get('function', '-'),
+        hosts=', '.join(response.get('hosts', [])))
+
+
+def restart_oneprovider(config):
+    users = get_users(config) + [('admin', 'password')]
+    print('Stopping services to be sure')
+    stop_request = do_request(users, requests.patch,
+                              'https://127.0.0.1:9443/api/v3/onepanel/provider/services?started=false',
+                              headers={'content-type': 'application/json'},
+                              verify=False)
+    if stop_request.status_code != 204:
+        errdata = json.loads(stop_request.text)
+        raise ValueError(format_error('stopping oneprovider', errdata))
+    print('Starting services')
+    start_request = do_request(users, requests.patch,
+                               'https://127.0.0.1:9443/api/v3/onepanel/provider/services?started=true',
+                               headers={'content-type': 'application/json'},
+                               verify=False)
+    if start_request.status_code != 204:
+        errdata = json.loads(stop_request.text)
+        raise ValueError(format_error('starting oneprovider', errdata))
+
 
 if __name__ == '__main__':
     try:
@@ -303,9 +347,17 @@ if __name__ == '__main__':
 
         start_onepanel()
 
-        configured = False
+        configured = is_configured(batch_config)
         batch_mode = os.environ.get('ONEPANEL_BATCH_MODE', 'false')
-        if batch_mode.lower() == 'true':
+
+        print('configured: {}'.format(configured))
+
+        if configured:
+            print('Starting existing oneprovider')
+            restart_oneprovider(batch_config)
+            configured = True
+        elif batch_mode.lower() == 'true':
+            print('Configuring oneprovider')
             configure(batch_config)
             configured = True
 
