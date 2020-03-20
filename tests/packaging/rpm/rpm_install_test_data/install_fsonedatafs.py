1bd3d3d6cd7fa2cf66cf380b31a385a615b30b9d
Darin Nikolow <darnik22@gmail.com>
2020-03-20 10:53:43 +0000

    Updated dockers and refs for 2002

--- tests/packaging/rpm/rpm_install_test_data/install_fsonedatafs.py
+++ tests/packaging/rpm/rpm_install_test_data/install_fsonedatafs.py
@@ -5,16 +5,16 @@ from subprocess import STDOUT, check_call, check_output
 packages = check_output(['ls', '/root/pkg']).split()
 packages = sorted(packages, reverse=True)
 onedatafs_py2_package = [path for path in packages
-                         if path.startswith('onedata{release}-python2-onedatafs'.format(release='1909'))][0]
+                         if path.startswith('onedata{release}-python2-onedatafs'.format(release='2002'))][0]
 
 onedatafs_py3_package = [path for path in packages
-                         if path.startswith('onedata{release}-python3-onedatafs'.format(release='1909'))][0]
+                         if path.startswith('onedata{release}-python3-onedatafs'.format(release='2002'))][0]
 
 fsonedatafs_py2_package = [path for path in packages
-                         if path.startswith('onedata{release}-python2-fs-onedatafs'.format(release='1909'))][0]
+                         if path.startswith('onedata{release}-python2-fs-onedatafs'.format(release='2002'))][0]
 
 fsonedatafs_py3_package = [path for path in packages
-                         if path.startswith('onedata{release}-python3-fs-onedatafs'.format(release='1909'))][0]
+                         if path.startswith('onedata{release}-python3-fs-onedatafs'.format(release='2002'))][0]
 
 # install Python prerequisites
 check_call(['yum', '-y', 'install', 'python2-pip', 'python36-pip'])
@@ -38,9 +38,9 @@ check_call(['yum', '-y', '--enablerepo=onedata', 'install',
             '/root/pkg/' + fsonedatafs_py3_package], stderr=STDOUT)
 
 # validate onedatafs Python2 package installation
-check_call(['scl', 'enable', 'onedata{release}'.format(release='1909'), 'PYTHONPATH="${ONEDATA_PYTHON2_PATH}" python2 -c "from fs.onedatafs import OnedataFS"'])
+check_call(['scl', 'enable', 'onedata{release}'.format(release='2002'), 'PYTHONPATH="${ONEDATA_PYTHON2_PATH}" python2 -c "from fs.onedatafs import OnedataFS"'])
 
 # validate onedatafs Python3 package installation
-check_call(['scl', 'enable', 'onedata{release}'.format(release='1909'), 'PYTHONPATH="${ONEDATA_PYTHON3_PATH}" python3 -c "from fs.onedatafs import OnedataFS"'])
+check_call(['scl', 'enable', 'onedata{release}'.format(release='2002'), 'PYTHONPATH="${ONEDATA_PYTHON3_PATH}" python3 -c "from fs.onedatafs import OnedataFS"'])
 
 sys.exit(0)
