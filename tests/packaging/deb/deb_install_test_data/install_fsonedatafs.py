c49d660dccb9db96b85d2a9bcd20dc25f768d5be
Bartek Kryza <bkryza@gmail.com>
2021-04-07 11:31:15 +0200

    VFS-7238 Updated package test scripts

--- tests/packaging/deb/deb_install_test_data/install_fsonedatafs.py
+++ tests/packaging/deb/deb_install_test_data/install_fsonedatafs.py
@@ -24,28 +24,24 @@ fsonedatafs_py3_package = [path for path in packages
                          and (dist in path)][0]
 
 # install Python prerequisites
-check_call(['apt-get', '-y', 'install', 'python-pip', 'python3-pip'])
+check_call(['apt', '-y', 'install', 'python-pip', 'python3-pip'])
 check_call(['pip', 'install', 'setuptools', 'six', 'fs'])
 check_call(['pip3', 'install', 'setuptools', 'six', 'fs'])
 
 # install onedatafs Python2 package
-check_call(['sh', '-c', 'dpkg -i /root/pkg/{package} ; apt-get -f -y '
-                        'install'.format(package=onedatafs_py2_package)
+check_call(['sh', '-c', 'apt -y install /root/pkg/{package}'.format(package=onedatafs_py2_package)
             ], stderr=STDOUT)
 
 # install onedatafs Python3 package
-check_call(['sh', '-c', 'dpkg -i /root/pkg/{package} ; apt-get -f -y '
-                        'install'.format(package=onedatafs_py3_package)
+check_call(['sh', '-c', 'apt -y install /root/pkg/{package}'.format(package=onedatafs_py3_package)
             ], stderr=STDOUT)
 
 # install fs-onedatafs Python2 package
-check_call(['sh', '-c', 'dpkg -i /root/pkg/{package} ; apt-get -f -y '
-                        'install'.format(package=fsonedatafs_py2_package)
+check_call(['sh', '-c', 'apt -y install /root/pkg/{package}'.format(package=fsonedatafs_py2_package)
             ], stderr=STDOUT)
 
 # install fs-onedatafs Python3 package
-check_call(['sh', '-c', 'dpkg -i /root/pkg/{package} ; apt-get -f -y '
-                        'install'.format(package=fsonedatafs_py3_package)
+check_call(['sh', '-c', 'apt -y install /root/pkg/{package}'.format(package=fsonedatafs_py3_package)
             ], stderr=STDOUT)
 
 # validate fs-onedatafs Python2 package installation
